911f1d962f72a828f061a6af092a094cda3c2b08
pigeoner <2390730311@qq.com>
2019-05-08 00:00:06 +0800

    啊！随便打的

---
+++ text-file-process/count.py
@@ -0,0 +1,12 @@
+import re
+count=0
+mynum=['201811113013']
+pattern=re.compile(r'(?<=学号：)\w+(?=, 时间：)')
+with open('log_files/201811113013.log',encoding='utf8') as f:
+    for line in f:
+        result=pattern.findall(line)
+        if result==mynum:
+            count=count+1    
+print(count)
+# 结果是109
+        
