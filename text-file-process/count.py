9f65ce82e16353e0de8a0464d8bca9f3c52344d7
pigeoner <2390730311@qq.com>
2019-05-11 22:22:36 +0800

    我又又改了一下

--- text-file-process/count.py
+++ text-file-process/count.py
@@ -1,10 +1,9 @@
 ###学号201811113013###
 import re
 result=[]
-pattern=re.compile(r'(?<=学号：)\w+(?=, 时间：)')
 with open('log_files/201811113013.log',encoding='utf8') as f:
     for line in f:
-        result.append(pattern.findall(line)[0])
+        result.append(re.split(r'[：,]',line)[3])
 print(result.count('201811113013'))
 # 结果是109
         
